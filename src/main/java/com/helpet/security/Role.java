e7257aa6f714a39157bd503c49dfa3afa5b74110
Efim Nikitin <e.nikitin1@g.nsu.ru>
2022-11-27 16:38:34 +0700

    Add roles

---
+++ src/main/java/com/helpet/security/Role.java
@@ -0,0 +1,13 @@
+package com.helpet.security;
+
+public class Role {
+    public static final String ADMIN = "ADMIN";
+
+    public static final String MODERATOR = "MODERATOR";
+
+    public static final String VET = "VET";
+
+    public static final String USER = "USER";
+
+    private Role() {}
+}
